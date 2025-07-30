d021bf96532ff04f8517a0722cacd08eac0ee6f7
Gema Rodríguez Jorge <gema_rj@hotmail.com>
2025-07-30 11:35:36 +0200

    Update 00_Introduccion.py

--- 00_Introduccion.py
+++ 00_Introduccion.py
@@ -1,1 +1,22 @@
 print("Hola desde GitHub y VSCode 🚀")
+
+# Imprimir por consola:
+
+print("Hola Python")
+
+# Comentarios:
+# Misma línea: Esto es un comentario
+# Varías línea: 
+"""
+Este es un comentario
+en varias
+líneas
+"""
+
+# Consultar el Tipo de Dato:
+
+print (type("Soy un dato tipo str")) #Tipo de dato string
+print (type(5)) #Tipo de dato int
+print (type(1.5)) #Tipo de dato float
+print (type("True")) #Tipo de dato bool
+print (type(3+ 1j)) #Tipo de dato complejo
