3d012675eb20cdfc34fb69292d46f5d1b2543d25
GemaRJ <gema_rj@hotmail.com>
2025-10-23 18:44:58 +0200

    ACTUAKIZAR

--- 00_Introduccion.py
+++ 00_Introduccion.py
@@ -1,6 +1,7 @@
 print("Hola desde GitHub y VSCode 🚀")
 
 # Imprimir por consola:
+# ENTRADA DE DATOS:
 
 print("Hola Python")
 
@@ -20,3 +21,13 @@ print (type(5)) #Tipo de dato int
 print (type(1.5)) #Tipo de dato float
 print (type("True")) #Tipo de dato bool
 print (type(3+ 1j)) #Tipo de dato complejo
+
+#SALIDA DE DATOS:
+nombre = "Juan"
+correo = "juan@gmail.com"
+print(f"El correo de {nombre} es {correo}, el cual cuenta con {len(correo)} caracteres")
+
+#PEDIR DATOS POR CONSOLA:
+nombre = input("¿Cuál es tu nombre? ")
+correo = input("¿Cuál es tu correo? ")
+edad = input("¿Cuál es tu edad? ")
