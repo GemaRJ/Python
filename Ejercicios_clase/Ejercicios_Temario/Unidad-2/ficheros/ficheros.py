ecf3bdeb57d463569284995d3f86e174d028b916
GemaRJ <gema_rj@hotmail.com>
2026-02-27 12:32:51 +0100

    añadir enunciado

--- Ejercicios_clase/Ejercicios_Temario/Unidad-2/ficheros/ficheros.py
+++ Ejercicios_clase/Ejercicios_Temario/Unidad-2/ficheros/ficheros.py
@@ -1,3 +1,15 @@
+"""  Leer todas las líneas y procesarlas una a una.
+▪ Para cada línea
+o Separar el nombre y la nota.
+o Intentar convertir la nota a float.
+o Si la conversión falla (por ejemplo, si aparece mal en lugar de una nota válida),
+capturar la excepción y mostrar un mensaje de aviso con el nombre del
+alumno.
+▪ Escribir en un nuevo fichero llamado notas_validas.txt solo las líneas con notas
+válidas.
+Durante el programa debes utilizar bloques try-except para controlar los posibles
+errores (como errores de conversión o si el fichero original no existe). """
+
 def leer_notas():
     try:
         # Abrimos el fichero en modo lectura
